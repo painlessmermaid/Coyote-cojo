997473c8686aa656be9ca55445bc12cb2316f89b
Braulio Daniel <planblocos@hotmail.com>
2022-08-06 20:35:11 -0500

    new

---
+++ pages/home/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import styles from '../../styles/Coyote.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+/* import {Card , Cardimg} from 'reactstrap'; */
+
+
+export default function Home() {
+  return (
+
+
+    <div className={styles.container}>
+      <Head>
+        <title>Coyote-cojo</title>
+      </Head>
+     <div>
+      
+     </div>
+
+    </div>
+  )
+}
